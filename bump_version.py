9873932de043f178f0d32693445af7d859ecf65c
Stanley <122285968+stanley-ubidata@users.noreply.github.com>
2024-10-28 17:32:05 +0100

    Enhance version bumping functionality and update version format
    - Added a step to get the latest tag based on the environment name in bumping.yml
    - Updated version from 1.2.4+124 to 9.0.10+1 in VERSION file
    - Modified bump_version function to accept additional parameters for major, minor, and patch version increments
    - Improved command-line argument handling in bump_version.py

--- bump_version.py
+++ bump_version.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-def bump_version(version):
+def bump_version(version, super_major="-", super_minor="-", super_patch="-"):
     # Split the version into version part and build number
     version_parts = version.split('+')
     version_part = version_parts[0]
@@ -13,11 +13,19 @@ def bump_version(version):
     minor = int(version_numbers[1])
     patch = int(version_numbers[2])
 
-    # Increment the patch and build numbers
-    patch += 1
-    build_number += 1
+    build_number += 1    
 
-    # Form the new version string
+    if super_major != "-":
+        major = super_major
+        minor = super_minor != "-" and super_minor or 0
+        patch = super_patch != "-" and super_patch or 0
+    elif super_minor != "-":
+        minor = super_minor
+        patch = super_patch != "-" and super_patch or 0
+    else:
+        patch = super_patch != "-" and super_patch or patch+1
+
+    # Create the new version string
     new_version = f"{major}.{minor}.{patch}+{build_number}"
 
     # Navigate to the folder on top
@@ -33,7 +41,19 @@ def bump_version(version):
         print(version_file.read())
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 1:
         print("Please provide a version number as a command line argument.")
-    else:
+    elif len(sys.argv) == 2:
         bump_version(sys.argv[1])
+    elif len(sys.argv) == 3:
+        bump_version(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:
+        bump_version(sys.argv[1], sys.argv[2], sys.argv[3])
+    elif len(sys.argv) == 5:
+        bump_version(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    else:
+        print("Too many arguments provided.")
+
+# Run the script
+# python bump_version.py 1.0.0+1
+
